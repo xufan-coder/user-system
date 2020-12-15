497eea80a74e3c642755b24dc54f13598d3f6aa3
dabai <854965206@qq.com>
2020-12-15 15:51:22 +0800

    初始化项目

--- src/main/java/com/zerody/user/UserSystemApplication.java
+++ src/main/java/com/zerody/user/UserSystemApplication.java
@@ -1,20 +1,14 @@
-package com.whzc.user;
+package com.zerody.user;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@EnableAutoConfiguration//启用自动配置
 @EnableDiscoveryClient
-@EnableEurekaClient
 @ComponentScan(basePackages = { "com.zerody.user.**" })
 @MapperScan("com.zerody.user.mapper")
 @EnableCaching
