fa1ba7d07b263f9adf43bb179cc525e469283f62
dabai <854965206@qq.com>
2021-01-08 16:10:52 +0800

    后台管理员企业

--- src/main/java/com/zerody/user/service/impl/CompanyAdminServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/CompanyAdminServiceImpl.java
@@ -1,11 +1,17 @@
 package com.zerody.user.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zerody.common.exception.DefaultException;
+import com.zerody.common.utils.DataUtil;
 import com.zerody.user.domain.CompanyAdmin;
+import com.zerody.user.domain.SysStaffInfo;
 import com.zerody.user.mapper.CompanyAdminMapper;
+import com.zerody.user.mapper.SysStaffInfoMapper;
 import com.zerody.user.service.CompanyAdminService;
+import com.zerody.user.service.SysStaffInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 /**
  * 
@@ -16,9 +22,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CompanyAdminServiceImpl extends ServiceImpl<CompanyAdminMapper, CompanyAdmin> implements CompanyAdminService {
 	private static final Logger log = LoggerFactory.getLogger(CompanyAdminServiceImpl.class);
-	
+	@Autowired
+	private SysStaffInfoMapper sysStaffInfoMapper;
 	@Override
 	public CompanyAdmin addCompanyAdmin(CompanyAdmin data) throws Exception {
+		SysStaffInfo sysStaffInfo = sysStaffInfoMapper.selectById(data.getStaffId());
+		if (DataUtil.isEmpty(sysStaffInfo)){
+			throw new DefaultException("员工不存在！");
+		}
+		data.setCompanyId(sysStaffInfo.getCompId());
 		this.save(data);
 		return data;
 	}
