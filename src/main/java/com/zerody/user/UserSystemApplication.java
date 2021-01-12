11089ee7b085ce84b210dfe4feff449f01a187ad
dabai <854965206@qq.com>
2021-01-12 09:39:31 +0800

    增加定时任务启动注解，增加日志

--- src/main/java/com/zerody/user/UserSystemApplication.java
+++ src/main/java/com/zerody/user/UserSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -14,6 +15,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.zerody.user.mapper")
 @EnableCaching
 @EnableFeignClients(basePackages= {"com.zerody.**"})
+@EnableScheduling
 public class UserSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserSystemApplication.class, args);
--- src/main/java/com/zerody/user/task/BuildVisitNoticeTask.java
+++ src/main/java/com/zerody/user/task/BuildVisitNoticeTask.java
@@ -1,6 +1,7 @@
 package com.zerody.user.task;
 
 import com.zerody.user.service.TaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,13 +12,17 @@ import org.springframework.stereotype.Component;
  * @date  2021/1/11 17:38
  */
 @Component
+@Slf4j
 public class BuildVisitNoticeTask {
 
     @Autowired
     private TaskService taskService;
 
+
+
     @Scheduled(cron = "0 0 1 * * ?")
     public void buildVisitNoticeInfo() {
+        log.info("进入客户跟进消息提醒任务;");
         taskService.buildVisitNoticeInfo();
     }
 }
