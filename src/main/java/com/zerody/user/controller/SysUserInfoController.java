680b31cc966bddfb488c0e6063d41428e49072a5
dabai <854965206@qq.com>
2021-01-18 18:18:54 +0800

    修改用户基础信息，修改命名规范

--- src/main/java/com/zerody/user/controller/SysUserInfoController.java
+++ src/main/java/com/zerody/user/controller/SysUserInfoController.java
@@ -36,7 +36,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
  * @Deacription TODO
  */
 @Slf4j
-@RequestMapping("/sysUserInfo")
+@RequestMapping("/sys-user-info")
 @RestController
 public class SysUserInfoController implements UserRemoteService {
 
