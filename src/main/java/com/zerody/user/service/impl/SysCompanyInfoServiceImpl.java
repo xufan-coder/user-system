d70010841a83726b720c763bcb01e4090b20dfe6
pengqiang <pengqiangvip@vip.qq.com>
2021-01-27 16:15:34 +0800

    添加员工时 根据手机号判断是否有名片 有的话改所属人 --解决冲突

--- src/main/java/com/zerody/user/service/impl/SysCompanyInfoServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/SysCompanyInfoServiceImpl.java
@@ -130,72 +130,6 @@ public class SysCompanyInfoServiceImpl extends BaseService<SysCompanyInfoMapper,
         sysCompanyInfo.setStatus(StatusEnum.激活.getValue());
         log.info("B端添加企业入库参数--{}", JSON.toJSONString(sysCompanyInfo));
         this.saveOrUpdate(sysCompanyInfo);
-        //生成用户
-        SysUserInfo userInfo = new SysUserInfo();
-        userInfo.setPhoneNumber(sysCompanyInfo.getContactPhone());
-        userInfo.setUserName(sysCompanyInfo.getContactName());
-        userInfo.setStatus(StatusEnum.激活.getValue());
-        userInfo.setCreateId(UserUtils.getUserId());
-        userInfo.setCreateTime(new Date());
-        userInfo.setCreateUser(UserUtils.getUserName());
-        userInfo.setId(UUIDutils.getUUID32());
-        sysUserInfoMapper.insert(userInfo);
-        //企业生成成功生成企业管理员登录账号
-        SysLoginInfo sysLoginInfo = new SysLoginInfo();
-        sysLoginInfo.setMobileNumber(sysCompanyInfo.getContactPhone());
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String initPwd = SysStaffInfoService.getInitPwd();
-        sysLoginInfo.setUserPwd(passwordEncoder.encode(MD5Utils.MD5( initPwd )));
-        sysLoginInfo.setUserId(userInfo.getId());
-        sysLoginInfo.setCreateTime(new Date());
-        sysLoginInfo.setCreateId(UserUtils.getUserId());
-        sysLoginInfo.setStatus(StatusEnum.激活.getValue());
-        sysLoginInfoMapper.insert(sysLoginInfo);
-        SysStaffInfo staff = new SysStaffInfo();
-        staff.setId(UUIDutils.getUUID32());
-        staff.setCompId(sysCompanyInfo.getId());
-        staff.setUserId(userInfo.getId());
-        staff.setUserName(userInfo.getUserName());
-        staff.setUserId(userInfo.getId());
-        staff.setCreateTime(new Date());
-        staff.setCreateId(UserUtils.getUserId());
-        staff.setStatus(StatusEnum.激活.getValue());
-        staff.setDeleted(YesNo.NO);
-        this.sysStaffInfoMapper.insert(staff);
-        this.saveOrUpdate(sysCompanyInfo);
-//        //生成用户
-//        SysUserInfo userInfo = new SysUserInfo();
-//        userInfo.setPhoneNumber(sysCompanyInfo.getContactPhone());
-//        userInfo.setUserName(sysCompanyInfo.getContactName());
-//        userInfo.setStatus(StatusEnum.激活.getValue());
-//        userInfo.setCreateId(UserUtils.getUserId());
-//        userInfo.setCreateTime(new Date());
-//        userInfo.setCreateUser(UserUtils.getUserName());
-//        userInfo.setId(UUIDutils.getUUID32());
-//        sysUserInfoMapper.insert(userInfo);
-//        //企业生成成功生成企业管理员登录账号
-//        SysLoginInfo sysLoginInfo = new SysLoginInfo();
-//        sysLoginInfo.setMobileNumber(sysCompanyInfo.getContactPhone());
-//        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        String initPwd = SysStaffInfoService.getInitPwd();
-//        sysLoginInfo.setUserPwd(passwordEncoder.encode(MD5Utils.MD5( initPwd )));
-//        sysLoginInfo.setUserId(userInfo.getId());
-//        sysLoginInfo.setCreateTime(new Date());
-//        sysLoginInfo.setCreateId(UserUtils.getUserId());
-//        sysLoginInfo.setStatus(StatusEnum.激活.getValue());
-//        sysLoginInfoMapper.insert(sysLoginInfo);
-//        SysStaffInfo staff = new SysStaffInfo();
-//        staff.setId(UUIDutils.getUUID32());
-//        staff.setCompId(sysCompanyInfo.getId());
-//        staff.setUserId(userInfo.getId());
-//        staff.setUserName(userInfo.getUserName());
-//        staff.setUserId(userInfo.getId());
-//        staff.setCreateTime(new Date());
-//        staff.setCreateId(UserUtils.getUserId());
-//        staff.setDeleted(YesNo.NO);
-//        staff.setStatus(StatusEnum.激活.getValue());
-//        this.sysStaffInfoMapper.insert(staff);
-//        this.saveOrUpdate(sysCompanyInfo);
         SetSysUserInfoDto userInfoDto = new SetSysUserInfoDto();
         userInfoDto.setCompanyId(sysCompanyInfo.getId());
         userInfoDto.setPhoneNumber(sysCompanyInfo.getContactPhone());
@@ -212,17 +146,7 @@ public class SysCompanyInfoServiceImpl extends BaseService<SysCompanyInfoMapper,
         this.companyAdminMapper.insert(admin);
         //预设管理员角色
         this.oauthFeignService.addCompanyRole(sysCompanyInfo.getId());
-//        this.saveCardUser(userInfo, sysLoginInfo, sysCompanyInfo);
-//        SmsDto smsDto=new SmsDto();
-//        smsDto.setMobile(userInfo.getPhoneNumber());
-//        Map<String, Object> content=new HashMap<>();
-//        content.put("userName",userInfo.getPhoneNumber());
-//        content.put("passWord",initPwd);
-//        smsDto.setContent(content);
-//        smsDto.setTemplateCode(userTipTemplate);
-//        smsDto.setSign(smsSign);
-//        //发送短信
-//        smsFeignService.sendSms(smsDto);
+
     }
 
 
