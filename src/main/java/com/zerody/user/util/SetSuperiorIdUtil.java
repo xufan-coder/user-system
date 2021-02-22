1998db0ab06fc5836c22b3ad24de1d563714e641
pengqiang <pengqiangvip@vip.qq.com>
2021-02-22 15:27:34 +0800

    添加查询上级部门接口

---
+++ src/main/java/com/zerody/user/util/SetSuperiorIdUtil.java
@@ -0,0 +1,47 @@
+package com.zerody.user.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author PengQiang
+ * @ClassName SetSuperiorIdUtil
+ * @DateTime 2021/2/22_14:37
+ * @Deacription TODO
+ */
+public class SetSuperiorIdUtil {
+
+    private final static String SUPERIOR_SYMBOL  = "_";
+
+    /**
+     *
+     *
+     * @author               PengQiang
+     * @description          获取上级id
+     * @date                 2021/2/22 14:57
+     * @param                [id]
+     * @return               java.util.List<java.lang.String>
+     */
+    public static List<String> getSuperiorIds(String id){
+        return getSuperiorIds(id, new ArrayList<>());
+    }
+
+    /**
+     *
+     *
+     * @author               PengQiang
+     * @description          获取上级id ( 第一个为自身id 如果不要自身的id 可remove(0)
+     * @date                 2021/2/22 14:54
+     * @param                [id, superiorIds]
+     * @return               java.util.List<java.lang.String>
+     */
+    private static List<String> getSuperiorIds(String id, List<String> superiorIds){
+        superiorIds.add(id);
+        if(id.lastIndexOf(SUPERIOR_SYMBOL) == -1){
+            return superiorIds;
+        }
+        id = id.substring(0, id.lastIndexOf(SUPERIOR_SYMBOL));
+        return getSuperiorIds(id, superiorIds);
+    }
+
+}
