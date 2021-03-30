66a523f3b0c86add0d834fb85ed7260b0fcc5b9b
huanghuasheng <632261049@qq.com>
2021-03-30 16:36:08 +0800

    删除过期消息

--- src/main/java/com/zerody/user/task/BuildVisitNoticeTask.java
+++ src/main/java/com/zerody/user/task/BuildVisitNoticeTask.java
@@ -1,10 +1,14 @@
 package com.zerody.user.task;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import com.zerody.user.service.SysUserInfoService;
 import com.zerody.user.service.TaskService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,12 +24,18 @@ public class BuildVisitNoticeTask {
 
     @Autowired
     private TaskService taskService;
-
+	@Autowired
+	SysUserInfoService sysUserInfoService;
 
     @XxlJob("follow_up_message")
     public ReturnT<String> execute(String param){
         log.info("进入客户跟进消息提醒任务;");
-        taskService.buildVisitNoticeInfo();
+		// 获取所有用户ID
+		List<Map<String, String>> list = sysUserInfoService.selectAllUserId();
+		// 统计客户跟进提醒三种类型的，客户数
+		for (Map<String, String> user : list) {
+			taskService.buildVisitNoticeInfo(user);
+		}
         return ReturnT.SUCCESS;
     }
 }
