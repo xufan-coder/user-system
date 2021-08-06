16ee39bfd884e7c85139642360c9d599af8e325a
dabai <854965206@qq.com>
2021-08-06 11:22:09 +0800

    增加审批状态和离职

--- src/main/java/com/zerody/user/service/impl/ResignationApplicationServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/ResignationApplicationServiceImpl.java
@@ -4,11 +4,15 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zerody.common.constant.YesNo;
+import com.zerody.common.enums.StatusEnum;
 import com.zerody.common.utils.DataUtil;
 import com.zerody.user.domain.ResignationApplication;
 import com.zerody.user.dto.ResignationPageDto;
+import com.zerody.user.enums.ApproveStatusEnum;
 import com.zerody.user.mapper.ResignationApplicationMapper;
 import com.zerody.user.service.ResignationApplicationService;
+import com.zerody.user.service.SysStaffInfoService;
 import com.zerody.user.service.base.CheckUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,10 +28,14 @@ import java.util.Date;
 @Slf4j
 @Service
 public class ResignationApplicationServiceImpl extends ServiceImpl<ResignationApplicationMapper, ResignationApplication> implements ResignationApplicationService {
-
+    @Autowired
+    private SysStaffInfoService sysStaffInfoService;
     @Override
     public void addOrUpdateResignationApplication(ResignationApplication data) {
         if(DataUtil.isNotEmpty(data.getId())){
+            if(ApproveStatusEnum.SUCCESS.name().equals(data.getApprovalState())){
+                sysStaffInfoService.updateStaffStatus(data.getUserId(), StatusEnum.stop.getValue());
+            }
             this.updateById(data);
         }else {
             data.setCreateTime(new Date());
