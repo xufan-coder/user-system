32a3527c41de0cd35c76ff0cd80defd17aecc86d
dabai <854965206@qq.com>
2021-09-11 09:24:00 +0800

    增加菜单

---
+++ src/main/java/com/zerody/user/service/UserMenuService.java
@@ -0,0 +1,22 @@
+package com.zerody.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zerody.common.vo.UserVo;
+import com.zerody.user.domain.UserMenu;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author  DaBai
+ * @date  2021/9/10 15:15
+ */
+
+public interface UserMenuService extends IService<UserMenu> {
+
+
+
+    List<Map> getUserMenu(String userId);
+
+    void addOrUpdateUserMenu(List<Map<String, Object>> param, UserVo user);
+}
