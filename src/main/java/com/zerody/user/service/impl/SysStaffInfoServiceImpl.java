7733458d28496f3e20f79e1907fbfa1d569e5936
e <pengqiangvip@vip.qq.com>
2021-11-27 13:06:15 +0800

    修改用户名称

--- src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
@@ -410,6 +410,12 @@ public class SysStaffInfoServiceImpl extends BaseService<SysStaffInfoMapper, Sys
         sysUserInfo.setUpdateId(UserUtils.getUserId());
         String avatar = setSysUserInfoDto.getAvatar();
         sysUserInfo.setAvatar(null);
+        //  如果名称有修改就修改名称修改状态 用于定时任务发送MQ消息
+        if (!oldUserInfo.getUserName().equals(setSysUserInfoDto.getUserName())) {
+            sysUserInfo.setIsUpdateName(YesNo.YES);
+            this.checkUtil.removeUserToken(sysUserInfo.getId());
+            removeToken = !removeToken;
+        }
         sysUserInfoMapper.updateById(sysUserInfo);
         QueryWrapper<SysLoginInfo> loginQW = new QueryWrapper<>();
         loginQW.lambda().eq(SysLoginInfo::getUserId, sysUserInfo.getId());
@@ -430,12 +436,6 @@ public class SysStaffInfoServiceImpl extends BaseService<SysStaffInfoMapper, Sys
         }
         if (setSysUserInfoDto.getRecommendType().intValue() == 1 && setSysUserInfoDto.getRecommendId().equals(staff.getId())) {
             throw new DefaultException("不能选择自己为推荐人");
-        }
-        //  如果名称有修改就修改名称修改状态 用于定时任务发送MQ消息
-        if (!oldUserInfo.getUserName().equals(setSysUserInfoDto.getUserName())) {
-            sysUserInfo.setIsUpdateName(YesNo.YES);
-            this.checkUtil.removeUserToken(sysUserInfo.getId());
-            removeToken = !removeToken;
         }
         staff.setRecommendId(setSysUserInfoDto.getRecommendId());
         staff.setRecommendType(setSysUserInfoDto.getRecommendType());
