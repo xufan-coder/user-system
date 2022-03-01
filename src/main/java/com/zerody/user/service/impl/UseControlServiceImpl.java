8fec579a262a310e8aecbfbb8611b7487a597659
dabai <854965206@qq.com>
2022-03-01 16:40:03 +0800

    添加登录限制代码  实现类

--- src/main/java/com/zerody/user/service/impl/UseControlServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/UseControlServiceImpl.java
@@ -1,13 +1,27 @@
 package com.zerody.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zerody.common.constant.YesNo;
+import com.zerody.common.utils.DataUtil;
+import com.zerody.user.constant.CommonConstants;
 import com.zerody.user.domain.UseControl;
 import com.zerody.user.dto.UseControlDto;
+import com.zerody.user.dto.UseControlTimeDto;
 import com.zerody.user.mapper.UseControlMapper;
 import com.zerody.user.service.UseControlService;
+import com.zerody.user.vo.UseControlTimeVo;
 import com.zerody.user.vo.UseControlVo;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author  DaBai
@@ -17,14 +31,65 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UseControlServiceImpl extends ServiceImpl<UseControlMapper, UseControl> implements UseControlService {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Override
     public void addOrUpdate(UseControlDto param) {
-
+        //是否全局关闭
+        if(YesNo.YES== param.getCloseAll()){
+            //禁止使用系统
+            this.stringRedisTemplate.opsForValue().set(CommonConstants.SYS_CLOSE, String.valueOf(YesNo.YES));
+        }else {
+            this.stringRedisTemplate.delete(CommonConstants.SYS_CLOSE);
+        }
+        List<UseControlTimeDto> weekInfo = param.getWeekInfo();
+        //查询该企业配置
+        QueryWrapper<UseControl> qw =new QueryWrapper<>();
+        qw.lambda().eq(UseControl::getCompanyId,param.getCompanyId());
+        List<UseControl> list = this.list(qw);
+        if(DataUtil.isNotEmpty(list)){
+            qw.clear();
+            this.remove(qw);
+        }
+        list.clear();
+        for (UseControlTimeDto useControlTimeDto : weekInfo) {
+            UseControl useControl=new UseControl();
+            BeanUtils.copyProperties(useControlTimeDto,useControl);
+            useControl.setCompanyId(param.getCompanyId());
+            useControl.setUpdateTime(new Date());
+            list.add(useControl);
+        }
+        this.saveBatch(list);
     }
 
     @Override
     public UseControlVo getByCompany(String companyId) {
-        return null;
+        UseControlVo vo =new UseControlVo();
+        String s = this.stringRedisTemplate.opsForValue().get(CommonConstants.SYS_CLOSE);
+        vo.setCloseAll(DataUtil.isEmpty(s)?YesNo.NO:YesNo.YES);
+        //查询该企业配置
+        QueryWrapper<UseControl> qw =new QueryWrapper<>();
+        qw.lambda().eq(UseControl::getCompanyId,companyId);
+        List<UseControl> list = this.list(qw);
+
+        if(DataUtil.isNotEmpty(list)){
+            List<UseControlTimeVo> weekInfo=new ArrayList<>();
+            for (UseControl useControl : list) {
+                UseControlTimeVo timeVo=new UseControlTimeVo();
+                BeanUtils.copyProperties(useControl,timeVo);
+                weekInfo.add(timeVo);
+            }
+            vo.setWeekInfo(weekInfo);
+        }else {
+            for (int i = 0; i < 7; i++) {
+                UseControlTimeVo timeVo=new UseControlTimeVo();
+                timeVo.setEnable(YesNo.YES);
+                timeVo.setWeek(i+1);
+                timeVo.setStart(0);
+                timeVo.setEnd(24);
+            }
+        }
+        vo.setCompanyId(companyId);
+        return vo;
     }
 }
--- src/main/java/com/zerody/user/service/impl/UsersUseControlServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/UsersUseControlServiceImpl.java
@@ -1,12 +1,23 @@
 package com.zerody.user.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zerody.common.exception.DefaultException;
+import com.zerody.common.utils.DataUtil;
+import com.zerody.user.domain.Msg;
 import com.zerody.user.domain.UsersUseControl;
 import com.zerody.user.dto.UsersUseControlPageDto;
 import com.zerody.user.mapper.UsersUseControlMapper;
+import com.zerody.user.service.SysStaffInfoService;
+import com.zerody.user.service.SysUserInfoService;
 import com.zerody.user.service.UsersUseControlService;
+import com.zerody.user.vo.LoginUserInfoVo;
+import com.zerody.user.vo.SysUserInfoVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -18,18 +29,41 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsersUseControlServiceImpl extends ServiceImpl<UsersUseControlMapper, UsersUseControl> implements UsersUseControlService {
 
+    @Autowired
+    private SysStaffInfoService sysStaffInfoService;
     @Override
     public void addNameList(UsersUseControl param) {
-
+        QueryWrapper<UsersUseControl> qw =new QueryWrapper<>();
+        qw.lambda().eq(UsersUseControl::getUserId,param.getUserId());
+        qw.lambda().eq(UsersUseControl::getType,param.getType());
+        UsersUseControl one = this.getOne(qw);
+        if(DataUtil.isNotEmpty(one)){
+            throw new DefaultException("改伙伴已在名单中！");
+        }else {
+            SysUserInfoVo sysUserInfoVo = sysStaffInfoService.selectStaffByUserId(param.getUserId());
+            UsersUseControl usersUseControl=new UsersUseControl();
+            BeanUtils.copyProperties(param,usersUseControl);
+            usersUseControl.setDeptId(sysUserInfoVo.getDepartId());
+            usersUseControl.setDeptName(sysUserInfoVo.getDepartName());
+            usersUseControl.setUserName(sysUserInfoVo.getUserName());
+            usersUseControl.setMobile(sysUserInfoVo.getPhoneNumber());
+            this.save(usersUseControl);
+        }
     }
 
     @Override
     public void removeNameList(String id) {
-
+        this.removeById(id);
     }
 
     @Override
     public IPage<UsersUseControl> getPageList(UsersUseControlPageDto pageDto) {
-        return null;
+        Page<UsersUseControl> page = new Page<>();
+        page.setCurrent(pageDto.getCurrent());
+        page.setSize(pageDto.getPageSize());
+        QueryWrapper<UsersUseControl> qw = new QueryWrapper<>();
+        qw.lambda().eq(UsersUseControl::getType,pageDto.getType());
+        IPage<UsersUseControl> pageResult = this.page(page,qw);
+        return pageResult;
     }
 }
