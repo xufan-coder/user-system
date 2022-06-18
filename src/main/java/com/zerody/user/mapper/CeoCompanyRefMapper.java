7bda2445aeb1547f7e43df7216d6dbc13d673ede
pengqiang <pengqiangvip@vip.qq.com>
2022-06-18 11:38:28 +0800

    添加ceo mappper

---
+++ src/main/java/com/zerody/user/mapper/CeoCompanyRefMapper.java
@@ -0,0 +1,13 @@
+package com.zerody.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zerody.user.domain.CeoCompanyRef;
+
+/**
+ *@ClassName CeoCompanyRefMapper
+ *@author    PengQiang
+ *@DateTime  2022/6/18_10:50
+ *@Deacription TODO
+ */
+public interface CeoCompanyRefMapper extends BaseMapper<CeoCompanyRef> {
+}
