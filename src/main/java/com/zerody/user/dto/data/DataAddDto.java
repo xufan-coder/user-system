badf388ad874b309cca4ef6fda8fd8dcddd3f74e
pengqiang <pengqiangvip@vip.qq.com>
2022-09-15 15:09:35 +0800

    键值对

---
+++ src/main/java/com/zerody/user/dto/data/DataAddDto.java
@@ -0,0 +1,22 @@
+package com.zerody.user.dto.data;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @author PengQiang
+ * @ClassName DataAddDto
+ * @DateTime 2022/9/15_14:39
+ * @Deacription TODO
+ */
+@Data
+public class DataAddDto {
+
+    /** key */
+    @NotEmpty(message = "key为空了")
+    private String dataKey;
+
+    /** 值 */
+    private String dataValue;
+}
