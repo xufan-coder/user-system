041a3dc0e98afb4e74af6c72b663206c52b1a231
dabai <854965206@qq.com>
2022-11-10 17:30:56 +0800

    增加IM通知

--- src/main/java/com/zerody/user/service/impl/CallControlServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/CallControlServiceImpl.java
@@ -5,9 +5,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zerody.common.constant.YesNo;
 import com.zerody.common.enums.SysCodeEnum;
 import com.zerody.common.exception.DefaultException;
+import com.zerody.common.util.JsonUtils;
 import com.zerody.common.utils.DataUtil;
 import com.zerody.common.utils.DateUtil;
 import com.zerody.common.vo.UserVo;
+import com.zerody.expression.Expression;
+import com.zerody.im.api.dto.SendRobotMessageDto;
+import com.zerody.im.feign.SendMsgFeignService;
+import com.zerody.im.util.IM;
 import com.zerody.oauth.api.dto.RemoveTokenDto;
 import com.zerody.oauth.api.dto.SendTemplateDataDto;
 import com.zerody.user.constant.CommonConstants;
@@ -15,22 +20,18 @@ import com.zerody.user.domain.CallControl;
 import com.zerody.user.domain.CallUseControl;
 import com.zerody.user.domain.UseControl;
 import com.zerody.user.domain.UsersUseControl;
+import com.zerody.user.dto.FlowMessageDto;
 import com.zerody.user.dto.UseControlTimeDto;
 import com.zerody.user.enums.WeeKEnum;
 import com.zerody.user.feign.OauthFeignService;
-import com.zerody.user.service.CallControlRecordService;
-import com.zerody.user.service.CallUseControlService;
-import com.zerody.user.vo.CallControlTimeVo;
-import com.zerody.user.vo.CallControlVo;
-import com.zerody.user.vo.UseControlTimeVo;
-import com.zerody.user.vo.UseControlVo;
+import com.zerody.user.service.*;
+import com.zerody.user.vo.*;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import com.zerody.user.mapper.CallControlMapper;
-import com.zerody.user.service.CallControlService;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalTime;
@@ -48,6 +49,9 @@ public class CallControlServiceImpl extends ServiceImpl<CallControlMapper, CallC
     public static String call_tip = "亲爱的%s，目前您已经呼叫了%s个客户，" +
             "今日限制呼叫次数为%s个，如超过次数将限制您今天使用系统，" +
             "小藏提醒您今天辛苦了，注意劳逸结合，该休息了！";
+
+    public static String leader_tip = "伙伴【%s-%s】因触发呼叫限制，已被强制退出登录系统，为确保客户信息安全，请及时了解情况！";
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -59,6 +63,15 @@ public class CallControlServiceImpl extends ServiceImpl<CallControlMapper, CallC
 
     @Autowired
     private OauthFeignService oauthFeignService;
+
+    @Autowired
+    private CompanyAdminService companyAdminService;
+
+    @Autowired
+    private SysStaffInfoService sysStaffInfoService;
+
+    @Autowired
+    private SendMsgFeignService sendMsgFeignService;
 
     @Override
     public CallControlVo getByCompany(String companyId) {
@@ -115,14 +128,15 @@ public class CallControlServiceImpl extends ServiceImpl<CallControlMapper, CallC
     @Override
     public void submitCallControl(UserVo user) {
         //提交呼叫次数
-        String companyId = user.getCompanyId();
-        String userId = user.getUserId();
-        String userName = user.getUserName();
-        this.checkCallAuth(userId,companyId,userName);
+        this.checkCallAuth(user);
     }
 
 
-    public void checkCallAuth(String userId,String companyId,String userName) {
+    public void checkCallAuth(UserVo user) {
+        String companyId = user.getCompanyId();
+        String userId = user.getUserId();
+        String userName = user.getUserName();
+        String deptId = user.getDeptId();
         //1.校验此人是否在白名单
         QueryWrapper<CallUseControl> qw =new QueryWrapper<>();
         qw.lambda().eq(CallUseControl::getUserId,userId);
@@ -164,6 +178,9 @@ public class CallControlServiceImpl extends ServiceImpl<CallControlMapper, CallC
                         this.callControlRecordService.saveRecord(userId);
                         //SCRM不强制退出
                         oauthFeignService.removeToken(new RemoveTokenDto(userId,SysCodeEnum.ZERODY_SCRM_MINI.getCode()));
+                        //通知上级
+                        List<CompanyAdminVo> leaders = companyAdminService.getCompanyAdminByUserId(companyId, deptId);
+                        sendIm(leaders,user);
                     }
                 }else {
                     StringBuffer tip = new StringBuffer();
@@ -203,6 +220,29 @@ public class CallControlServiceImpl extends ServiceImpl<CallControlMapper, CallC
             // 如果没有key值，对他进行添加到redis中
             stringRedisTemplate.opsForValue().set(key,"1",time, TimeUnit.SECONDS);
             return 1;
+        }
+    }
+
+
+    public void sendIm(List<CompanyAdminVo> leaders,UserVo userVo){
+        SysUserInfoVo sysUserInfoVo = sysStaffInfoService.selectStaffByUserId(userVo.getUserId());
+        if(DataUtil.isNotEmpty(sysUserInfoVo)) {
+            String message = String.format(leader_tip,sysUserInfoVo.getUserName(),sysUserInfoVo.getDepartName());
+            for (CompanyAdminVo leader : leaders) {
+                FlowMessageDto dto = new FlowMessageDto();
+                // 标题
+                dto.setTitle("伙伴呼叫限制通知");
+                dto.setContent(message);
+
+                SendRobotMessageDto data = new SendRobotMessageDto();
+                data.setContent(message);
+                data.setTarget(leader.getUserId());
+                data.setContentPush(message);
+                data.setContentExtra(JsonUtils.toString(dto));
+                data.setType(1);
+                com.zerody.common.api.bean.DataResult<Long> result = this.sendMsgFeignService.send(data);
+                LOGGER.info(leader.getUserId()+"呼叫限制通知推送IM结果:{}", com.zerody.flow.client.util.JsonUtils.toString(result));
+            }
         }
     }
 }
