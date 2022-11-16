45dc404a198552a596aa677ec6f1a429194336fc
yechangwei <y1303552707@163.com>
2022-11-16 20:51:42 +0800

    修改 获取入职周年的信息

--- src/main/java/com/zerody/user/controller/UserBirthdayTemplateController.java
+++ src/main/java/com/zerody/user/controller/UserBirthdayTemplateController.java
@@ -6,16 +6,10 @@ import com.zerody.common.api.bean.R;
 import com.zerody.common.constant.YesNo;
 import com.zerody.common.exception.DefaultException;
 import com.zerody.common.util.UserUtils;
-import com.zerody.contract.api.vo.SignOrderDataVo;
 import com.zerody.user.domain.BirthdayBlessing;
-import com.zerody.user.domain.UserBirthdayTemplate;
 import com.zerody.user.dto.BlessIngParam;
 import com.zerody.user.dto.TemplatePageDto;
 import com.zerody.user.dto.UserBirthdayTemplateDto;
-import com.zerody.user.feign.ContractFeignService;
-import com.zerody.user.feign.CustomerFeignService;
-import com.zerody.user.mapper.SysDepartmentInfoMapper;
-import com.zerody.user.mapper.SysUserInfoMapper;
 import com.zerody.user.service.BirthdayBlessingService;
 import com.zerody.user.service.UnionBirthdayMonthService;
 import com.zerody.user.service.UserBirthdayTemplateService;
@@ -27,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -47,17 +40,6 @@ public class UserBirthdayTemplateController {
     @Autowired
     private UnionBirthdayMonthService unionBirthdayMonthService;
 
-
-    @Autowired
-    private UserBirthdayTemplateService userBirthdayTemplateService;
-    @Autowired
-    private SysUserInfoMapper sysUserInfoMapper;
-    @Autowired
-    private SysDepartmentInfoMapper sysDepartmentInfoMapper;
-    @Autowired
-    private ContractFeignService contractFeignService;
-    @Autowired
-    private CustomerFeignService customerFeignService;
 
 
 
@@ -372,70 +354,25 @@ public class UserBirthdayTemplateController {
             return R.error("发送生日祝福错误" + e.getMessage());
         }
     }
-
-    private AppUserNotPushVo getEntryPullData(String userId){
-
-        //查询当前入职周年的信息
-        List<AppUserNotPushVo> lists = sysUserInfoMapper.getAnniversaryUserList(userId);
-        AppUserNotPushVo entryData;
-        if(lists.size() == 0) {
-           throw new DefaultException("查找周年庆错误");
+    /**
+    *
+    *  @description   查询入职周年的信息
+    *  @author        YeChangWei
+    *  @date          2022/11/16 20:42
+    *  @return        com.zerody.common.api.bean.DataResult<com.zerody.user.vo.AppUserNotPushVo>
+    */
+    @GetMapping("/get-entry-data")
+    public DataResult<AppUserNotPushVo> getEntryPullData(){
+        try {
+            AppUserNotPushVo appUserNotPushVo = this.service.getEntryPullData(UserUtils.getUserId());
+            return R.success(appUserNotPushVo);
+        } catch (DefaultException e) {
+            log.error("查询入职周年的信息错误：{}", e.getMessage());
+            return R.error(e.getMessage());
+        } catch (Exception e) {
+            log.error("查询入职周年的信息错误：{}", e, e);
+            return R.error("查询入职周年的信息错误！");
         }
-        entryData = lists.get(0);
-        //获取模板
-        UserBirthdayTemplate template = userBirthdayTemplateService.getEntryTimeTemplate(entryData.getNum().toString(),new Date(), YesNo.YES);
-        if(template == null) {
-            throw new DefaultException("未配置"+entryData.getNum()+"周年模板");
-        }
-        entryData.setBlessing(template.getBlessing());
-        entryData.setPosterUrl(template.getPosterUrl());
-        if (StringUtils.isNotEmpty(entryData.getCompanyId())) {
-            //总经理 查询该企业年统计量
-
-            //查询签单数量 和放款金额 和放款数
-            DataResult<SignOrderDataVo> signOrderData = contractFeignService.getSignOrderData(entryData.getCompanyId(), null, null);
-            if (signOrderData.isSuccess()) {
-                entryData.setSignOrderNum(signOrderData.getData().getSignOrderNum());
-                entryData.setLoansMoney(signOrderData.getData().getLoansMoney());
-                entryData.setLoansNum(signOrderData.getData().getLoansNum());
-            }
-            //查询录入客户数量
-            DataResult<Integer> importCustomerNum = customerFeignService.getImportCustomerNum(entryData.getCompanyId(), null, null);
-            if (importCustomerNum.isSuccess()) {
-                entryData.setImportCustomerNum(importCustomerNum.getData());
-            }
-        } else if (StringUtils.isNotEmpty(entryData.getDepartmentId())) {
-            //副总或团队长  查询该部门年统计量
-
-            //查询签单数量 和放款金额 和放款数
-            DataResult<SignOrderDataVo> signOrderData = contractFeignService.getSignOrderData(null, entryData.getDepartmentId(), null);
-            if (signOrderData.isSuccess()) {
-                entryData.setSignOrderNum(signOrderData.getData().getSignOrderNum());
-                entryData.setLoansMoney(signOrderData.getData().getLoansMoney());
-                entryData.setLoansNum(signOrderData.getData().getLoansNum());
-            }
-            //查询录入客户数量
-            DataResult<Integer> importCustomerNum = customerFeignService.getImportCustomerNum(null, entryData.getDepartmentId(), null);
-            if (importCustomerNum.isSuccess()) {
-                entryData.setImportCustomerNum(importCustomerNum.getData());
-            }
-
-        }else {
-            //伙伴数据
-            //查询签单数量 和放款金额 和放款数
-            DataResult<SignOrderDataVo> signOrderData = contractFeignService.getSignOrderData(null, null, userId);
-            if (signOrderData.isSuccess()) {
-                entryData.setSignOrderNum(signOrderData.getData().getSignOrderNum());
-                entryData.setLoansMoney(signOrderData.getData().getLoansMoney());
-                entryData.setLoansNum(signOrderData.getData().getLoansNum());
-            }
-            //查询录入客户数量
-            DataResult<Integer> importCustomerNum = customerFeignService.getImportCustomerNum(null, null, userId);
-            if (importCustomerNum.isSuccess()) {
-                entryData.setImportCustomerNum(importCustomerNum.getData());
-            }
-        }
-        entryData.setContent("亲爱的"+entryData.getUserName()+"小微集团祝您签约"+entryData.getNum()+"快乐");
-        return entryData;
     }
+
 }
--- src/main/java/com/zerody/user/service/UserBirthdayTemplateService.java
+++ src/main/java/com/zerody/user/service/UserBirthdayTemplateService.java
@@ -7,6 +7,7 @@ import com.zerody.user.domain.UserBirthdayTemplate;
 import com.zerody.user.dto.BlessIngParam;
 import com.zerody.user.dto.TemplatePageDto;
 import com.zerody.user.dto.UserBirthdayTemplateDto;
+import com.zerody.user.vo.AppUserNotPushVo;
 import com.zerody.user.vo.UserBirthdayTemplateVo;
 
 import java.util.Date;
@@ -47,4 +48,12 @@ public interface UserBirthdayTemplateService extends IService<UserBirthdayTempla
     *  @return        java.lang.Object
     */
     boolean whetherEntry(String userId);
+    /**
+    *
+    *  @description   查询入职周年的信息
+    *  @author        YeChangWei
+    *  @date          2022/11/16 20:45
+    *  @return        com.zerody.user.vo.AppUserNotPushVo
+    */
+    AppUserNotPushVo getEntryPullData(String userId);
 }
