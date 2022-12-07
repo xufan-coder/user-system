7c3bd9b7da107308cf2a2aac92df43535bf7f622
kuang <1909864685@qq.com>
2022-12-07 18:26:42 +0800

    修改

--- src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
@@ -264,12 +264,15 @@ public class SysStaffInfoServiceImpl extends BaseService<SysStaffInfoMapper, Sys
 
         if(StringUtils.isNotEmpty(setSysUserInfoDto.getTerminals()) &&
                 SystemCodeType.SYSTEM_CRM_PC.equals(setSysUserInfoDto.getTerminals())){
-
             LeaveUserInfoVo leave = sysStaffInfoMapper.getLeaveUserByPhone(setSysUserInfoDto.getPhoneNumber());
             if(leave != null){
                 throw new DefaultException("该伙伴是原["+leave.getCompanyName() +" —— "+ leave.getDepartName()+"]，不允许直接办理二次入职，" +
                         "请联系团队长在CRM-APP【伙伴签约申请】发起审批!");
             }
+
+            log.info("请求终端 ------ {}--------手机号---{}",setSysUserInfoDto.getTerminals(),setSysUserInfoDto.getPhoneNumber());
+        }else {
+            log.info("请求终端 ------ {}",setSysUserInfoDto.getTerminals());
         }
 
 
