4008e1bb60c09065e48bd6d16d95c14ae1787d7a
dabai <854965206@qq.com>
2022-12-08 17:28:12 +0800

    修改团队长副总错误问题2

--- src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
@@ -3016,26 +3016,25 @@ public class SysStaffInfoServiceImpl extends BaseService<SysStaffInfoMapper, Sys
         qw.lambda().eq(BaseModel::getId,staffInfo.getDepartId());
         //团队长
         SysDepartmentInfo leader = this.sysDepartmentInfoService.getOne(qw);
-        if(DataUtil.isEmpty(leader)){
-            return null;
-        }
-        SysStaffInfo byId = this.getById(leader.getAdminAccount());
-        if(DataUtil.isNotEmpty(byId)){
-            result.add(byId.getUserId());
+        if(DataUtil.isNotEmpty(leader)) {
+            SysStaffInfo byId = this.getById(leader.getAdminAccount());
+            if (DataUtil.isNotEmpty(byId)) {
+                result.add(byId.getUserId());
+            }
         }
 
         //副总
         qw.clear();
         qw.lambda().eq(BaseModel::getId,staffInfo.getDepartId().split("_")[0]);
         SysDepartmentInfo leader1 = this.sysDepartmentInfoService.getOne(qw);
-        if(DataUtil.isEmpty(leader1)){
-            return null;
-        }
         if(DataUtil.isNotEmpty(leader1)){
             SysStaffInfo byId1 = this.getById(leader1.getAdminAccount());
             if(DataUtil.isNotEmpty(byId1)){
                 result.add(byId1.getUserId());
             }
+        }
+        if(DataUtil.isEmpty(result)){
+            return null;
         }
         return result;
     }
