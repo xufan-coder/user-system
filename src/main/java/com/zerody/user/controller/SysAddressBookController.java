cd2f643e3e90b3d8cb9e606ab913a6c963da79c4
kuang <1909864685@qq.com>
2022-12-10 14:28:51 +0800

    删除日志

--- src/main/java/com/zerody/user/controller/SysAddressBookController.java
+++ src/main/java/com/zerody/user/controller/SysAddressBookController.java
@@ -257,8 +257,7 @@ public class SysAddressBookController {
                 }
                 staffByCompanyDto.setIsProData(byId.getIsProData());
             }
-            
-            log.info("/get/by-company   请求入参---{}", staffByCompanyDto);
+
             return R.success(sysAddressBookService.getStaffByCompany(staffByCompanyDto));
         } catch (DefaultException e) {
             log.error("获取员工错误:{}", JSON.toJSONString(staffByCompanyDto), e.getMessage());
