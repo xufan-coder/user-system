3f4bb9eaf48b1610f2556fd4c27602e65222cb8b
chenKeFeng <2806257319@qq.com>
2022-12-29 16:22:28 +0800

    推送消息加上注释

--- src/main/java/com/zerody/user/config/BirthdayMsgConfig.java
+++ src/main/java/com/zerody/user/config/BirthdayMsgConfig.java
@@ -6,30 +6,57 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
+ * 生日推送配置类
+ *
  * @author PengQiang
  * @ClassName ConsultingMsgConfig
  * @DateTime 2022/6/7_11:00
  * @Deacription TODO
  */
-@ConfigurationProperties(prefix="msg.birthday")
+@ConfigurationProperties(prefix = "msg.birthday")
 @Component
 @RefreshScope
 @Data
 public class BirthdayMsgConfig {
 
+    /**
+     * 跳转路径
+     */
     private String url;
 
-    /**生日推送路径*/
+    /**
+     * 生日推送路径
+     */
     private String birthdayUrl;
 
+    /**
+     * 路径参数
+     */
     private String query;
 
+    /**
+     * 内容参数
+     */
     private String arguments;
 
+    /**
+     * 标题1
+     */
     private String title;
 
+    /**
+     * 标题1
+     */
     private String title2;
 
+    /**
+     * 内容1
+     */
     private String content;
+
+    /**
+     * 内容2
+     */
     private String content2;
+
 }
