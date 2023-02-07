1fa663f1cbce7d602273f826e82aa689f7d00ebf
pengqiang <pengqiangvip@vip.qq.com>
2023-02-07 12:20:58 +0800

    修改为离职处理

--- src/main/java/com/zerody/user/handler/user/SysUserDimissionHandle.java
+++ src/main/java/com/zerody/user/handler/user/SysUserDimissionHandle.java
@@ -1,9 +1,18 @@
 package com.zerody.user.handler.user;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zerody.common.constant.YesNo;
+import com.zerody.common.enums.StatusEnum;
+import com.zerody.common.exception.DefaultException;
 import com.zerody.common.utils.DataUtil;
+import com.zerody.common.vo.UserVo;
 import com.zerody.user.domain.AppUserPush;
+import com.zerody.user.domain.ResignationApplication;
+import com.zerody.user.domain.SysUserInfo;
+import com.zerody.user.dto.SetSysUserInfoDto;
+import com.zerody.user.enums.ApproveStatusEnum;
 import com.zerody.user.service.AppUserPushService;
+import com.zerody.user.service.ResignationApplicationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,9 +37,16 @@ public class SysUserDimissionHandle {
 
     private static AppUserPushService appUserPushStaticService;
 
+    @Autowired
+    private ResignationApplicationService resignationApplicationService;
+
+
+    private static ResignationApplicationService resignationApplicationStaticService;
+
     @PostConstruct
     private void ini() {
         appUserPushStaticService = this.appUserPushService;
+        resignationApplicationStaticService = this.resignationApplicationService;
     }
 
 
@@ -54,5 +70,36 @@ public class SysUserDimissionHandle {
         appUserPush.setResigned(YesNo.YES);
         appUserPush.setUpdateTime(new Date());
         return appUserPush;
+    }
+
+
+    /**
+     *
+     * 判断是否可设置为离职
+     * @author               PengQiang
+     * @description          DELL
+     * @date                 2023/2/7 11:08
+     * @param                user 当前登录用户信息
+     * @param                userId 用户id
+     * @param                oldStatus 旧数据
+     * @param                newStatus 修改数据
+     * @return               void
+     */
+    public static void isDimission(UserVo user, String userId, int oldStatus, int newStatus) {
+        //token为空 或者 后台操作就直接通过
+        if (DataUtil.isEmpty(user) || user.isBack()) {
+            return;
+        }
+        if (DataUtil.isEmpty(newStatus) || newStatus == oldStatus) {
+            return;
+        }
+        QueryWrapper<ResignationApplication> resignationQw = new QueryWrapper<>();
+        resignationQw.lambda().eq(ResignationApplication::getUserId, userId);
+        resignationQw.lambda().eq(ResignationApplication::getApprovalState, ApproveStatusEnum.APPROVAL.name());
+        int num = resignationApplicationStaticService.count(resignationQw);
+        //当新的状态为离职 并且 该伙伴提交了离职申请未审批的
+        if (newStatus == StatusEnum.stop.getValue() && num > 0) {
+            throw new DefaultException("离职不可操作！当前伙伴已发起离职申请，请联系团队长审批。");
+        }
     }
 }
