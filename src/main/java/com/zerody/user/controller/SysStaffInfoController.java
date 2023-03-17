0cfa218253b81c700cb9cac60c8f7944254893d1
kuang <1909864685@qq.com>
2023-03-17 16:46:15 +0800

    跨企业二次入职申请-查询管理层优化

--- src/main/java/com/zerody/user/controller/SysStaffInfoController.java
+++ src/main/java/com/zerody/user/controller/SysStaffInfoController.java
@@ -631,7 +631,7 @@ public class SysStaffInfoController {
      * @author kuang
      */
     @GetMapping("/get/dept-leader")
-    public DataResult<List<String>> getDeptLeader(@RequestParam(name = "userId") String userId,@RequestParam(name = "leaderState",required = false) String signDeptId){
+    public DataResult<List<String>> getDeptLeader(@RequestParam(name = "userId") String userId,@RequestParam(name = "signDeptId",required = false) String signDeptId){
         try {
             List<String> map =sysStaffInfoService.getDeptLeader(userId,signDeptId);
             return R.success(map);
