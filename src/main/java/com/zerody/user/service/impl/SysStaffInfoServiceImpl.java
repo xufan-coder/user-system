244ada9f028c9e93dcb36d437ed619f98a6515ca
chenKeFeng <2806257319@qq.com>
2023-05-08 11:24:35 +0800

    伙伴统计优化4

--- src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
+++ src/main/java/com/zerody/user/service/impl/SysStaffInfoServiceImpl.java
@@ -3143,8 +3143,11 @@ public class SysStaffInfoServiceImpl extends BaseService<SysStaffInfoMapper, Sys
         Integer num = userStatistics.getContractNum();
         //二次签约
         List<SysStaffRelationVo> list = userInductionRecordService.statistics(param);
+        log.info("二次签约人数 {}", list);
+        log.info("transfer {}", this.transfer);
         Integer secondContractNum = 0;
         if (DataUtil.isNotEmpty(list)) {
+            log.info("二次签约人数进来 {}", list.size());
             List<SysStaffRelationVo> filterList = list.stream().filter(user -> !user.getLeaveType().equals(this.transfer)).collect(toList());
             secondContractNum = filterList.size();
         }
